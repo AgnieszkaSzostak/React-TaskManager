38bee636056098d5882e43867835ee8b945fd49c
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-12-07 17:45:53 +0100

    Add post, put and fetchData func

---
+++ src/components/TasksProvider.js
@@ -0,0 +1,32 @@
+
+const url = 'http://localhost:3005/tasks/'
+
+export function put(data, id = ''){
+    const options = {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+    }
+    return fetchData(options, id);
+}
+export function post (data){
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+    }
+    return fetchData(options);
+}
+
+function fetchData(options, id = ''){
+    const path = url + id;
+    const promise = fetch(path, options)
+    return promise 
+        .then(resp =>{
+            if(resp.ok){
+                return resp.json()
+            }
+            return Promise.reject(resp)
+        })
+        .catch(err => console.error(err))
+}
